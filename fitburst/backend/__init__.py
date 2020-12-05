13c346124c9225beb0d75f671a8b6a0978de8c38
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2020-12-05 14:08:22 -0500

    changed .yaml file name, now imported within backend init.

--- fitburst/backend/__init__.py
+++ fitburst/backend/__init__.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+import yaml, os
+
+config_data = yaml.load(
+    open(
+        "{0}/{1}".format(
+            os.path.dirname(__file__), 
+            "chime.yaml"
+        ), 
+        "r"
+    )
+)
