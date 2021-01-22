a72d50c67c2a284ac4d96d6120596d686220d9b9
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2021-01-22 15:47:30 -0800

    initial push of dedispersion method.

--- fitburst/utilities/bases.py
+++ fitburst/utilities/bases.py
@@ -1,3 +1,4 @@
+from fitburst.backend import general
 import fitburst.routines as rt
 import numpy as np
 import sys
@@ -13,14 +14,56 @@ class ReaderBaseClass(object):
         self.data_full = None
         self.data_weights = None
         self.data_windowed = None
+        self.dedispersion_idx = None
         self.freqs = None
         self.times = None
 
-    def dedisperse(self, dm):
-        "Returns data that are dedispersed at the supplied DM; to be defined by " + \
-        "inherited classes."
+    def dedisperse(self, 
+        dm: np.float, 
+        arrival_time: np.float,
+        reference_freq: np.float = np.inf,
+        dm_idx: np.float = general["constants"]["index_dispersion"]
+        ):
+        """
+        Computes a matrix of time-bin indices corresponding to delays from interstellar 
+        dispersion of the dynamic spectrum. 
+        
+        Parameters
+        ----------
+        dm : np.float
+            dispersion measure, in units of pc/cc.
 
-        pass
+        arrival_time : np.float
+            arrival time of the burst, in units of seconds.
+
+        reference_freq : np.float, optional
+            reference frequency for supplied arrival time.
+
+        dm_idx : np.float, optional
+            exponent of frequency dependence in dispersion delay
+
+        Returns
+        -------
+        self.dedispersion_delay : np.ndarray
+            a matrix of indeces corresponding to incoherent dedispersion of the raw data,
+            with dimensions of len(self.freqs) x len(self.times).
+        """
+
+        # initialize matrix that contains indices.
+        # the following code structure makes use of Numpy broadcasting.
+        dedispersion_idx = np.zeros(self.data_full.shape[::-1], dtype=np.int)
+
+        # now loop over dimensions and compute indices.
+        delay = arrival_time + rt.ism.compute_time_dm_delay(
+            dm,
+            self.freqs,
+            freq2=reference_freq,
+            dm_idx=dm_idx
+        )
+        
+        # fill initial matrix and transpose.
+        dedispersion_idx[:] = (delay - self.times[0]) / (self.times[1] - self.times[0])
+        self.dedispersion_idx = dedispersion_idx.T
 
     def load_data(self):
         "Loads data from file into memory; to be defined by inherited classes."
@@ -35,7 +78,30 @@ class ReaderBaseClass(object):
         ):
         """
         Applies pre-fit routines for cleaning raw dynamic spectrum (e.g., RFI-masking, 
-        baseline subtraction, normalization, etc.)
+        baseline subtraction, normalization, etc.).
+
+        Parameters
+        ----------
+        variance_range : list, optional
+            a two-element list containing the range of allowed variances; values outside 
+            of this range are flagged as RFI and removed from the data cube.
+
+        variance_weigt : np.float, optional
+            a scaling factor applied to variance prior to exision.
+
+        
+        skewness_range : list, optional
+            a two-element list containing the range of allowed values of skewness; 
+            values outside of this range are flagged as RFI and removed from the data cube.
+
+        Returns
+        -------
+        self.good_freqs : np.ndarray
+            an array of boolean values indicating good frequencies.
+
+        Notes
+        -----
+        This method normalizes and cleans the self.data_full cube.
         """
 
         # define weight and mask arrays.        
