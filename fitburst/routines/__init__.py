355cd75a636c9d6b209409c164ba949c41c5ad42
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2021-03-12 21:05:06 -0800

    initial push of full downsample functions.

--- fitburst/routines/__init__.py
+++ fitburst/routines/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 from . import ism
+from . import manipulate
 from . import profile
 from . import spectrum
--- fitburst/routines/manipulate.py
+++ fitburst/routines/manipulate.py
@@ -1,23 +1,40 @@
 import numpy as np
 
-def downsample(input_array, factor):
+def downsample_1d(array_orig: np.ndarray, factor: int, boolean: bool = False):
     """
     Downsamples an input array by the specified factor. It is assumed that the 
     input array is regularly sampled (i.e., the difference in adjacent bins 
     have the same value.)
     """
 
-    # TODO: flush this out.
-    pass
+    # reshape original array and marginalize.
+    num_elements = len(array_orig)
+    array_reshaped = np.reshape(array_orig, (num_elements // factor, factor))
+    array_downsampled = np.sum(array_reshaped, axis=1) / factor
 
-def downsample_spectrum(input_spectrum, input_freq, input_time, ):
+    if boolean:
+        array_downsampled = array_downsampled.astype(bool)
+
+    return array_downsampled
+
+def downsample_2d(spectrum_orig: np.ndarray, factor: int, axis: str = "freq"):
     """
     Downsamples a two-dimensional dynamic spectrum and its time/frequency arrays 
     by specified factors.
     """
 
-    # TODO: flush this out.
-    pass
+    # compute original and new matrix shapes.
+    num_freq, num_time = spectrum_orig.shape
+    shape_new = (num_freq // factor, factor, num_time)
+
+    if axis == "time":
+        shape_new = (num_freq, factor, num_time // factor)
+
+    # now reshape and average to downsample.
+    spectrum_reshaped = np.reshape(spectrum_orig.copy(), shape_new)
+    spectrum_downsampled = np.sum(spectrum_reshaped, axis=1) / factor
+
+    return spectrum_downsampled
 
 def upsample(input_array, factor):
     """
