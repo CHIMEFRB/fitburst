cff1ea6ff2edea80a04491059a9fdfed23cf1a3b
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2021-03-31 09:34:45 -0700

    added lines and replaced event ID to analyze multi-component burst.

--- fitburst/pipelines/fitburst_example_chimefrb.py
+++ fitburst/pipelines/fitburst_example_chimefrb.py
@@ -15,12 +15,14 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 # read in data.
-data = chimefrb.DataReader(37888771)
+# test "simple" burst: 37888771
+# test multi-component burst: 156410110
+data = chimefrb.DataReader(156410110)
 initial_parameters = data.get_parameters()
 
 # load data into memory and pre-process.
 data.load_data(data.files)
-data.preprocess_data(variance_weight=1/(const.L0_NUM_FRAMES_SAMPLE * 2))
+data.preprocess_data(variance_weight=1./(const.L0_NUM_FRAMES_SAMPLE * 2))
 
 # now compute dedisperse matrix for data, given initial DM, and grab windowed data.
 print("INFO: computing dedispersion-index matrix")
@@ -28,7 +30,7 @@ params = data.burst_parameters["fitburst"]["round_2"]
 data.dedisperse(
     params["dm"][0],
     params["arrival_time"][0],
-    reference_freq=params["reference_freq"]
+    reference_freq=params["reference_freq"][0]
 )
 
 data_windowed, times_windowed = data.window_data(params["arrival_time"][0], window=0.08)
@@ -36,6 +38,7 @@ data_windowed, times_windowed = data.window_data(params["arrival_time"][0], wind
 # now create initial model.
 print("INFO: initializing model")
 model = mod.SpectrumModeler()
+model.is_dedispersed = False
 model.set_dimensions(data.num_freq, len(times_windowed))
 model.set_dedispersion_idx(data.dedispersion_idx)
 model.update_parameters(initial_parameters)
@@ -54,7 +57,7 @@ model.update_parameters(bestfit_parameters)
 bestfit_model = model.compute_model(data.times, data.freqs)
 bestfit_residuals = data_windowed - bestfit_model
 
-ut.plotting.plot_summary(
-    data.times, data.freqs, data_windowed, fitter.good_freq, model = bestfit_model, 
+ut.plotting.plot_summary_triptych(
+    data.times, data.freqs, data_windowed, data.good_freq, model = bestfit_model, 
     residuals = bestfit_residuals
 )
