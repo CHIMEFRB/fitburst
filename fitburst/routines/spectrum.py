870e3ebe7f17804f32bf277a7ded1d9a0eeaecc5
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2022-03-07 10:23:35 -0500

    removed redundant Gaussian function

--- fitburst/routines/spectrum.py
+++ fitburst/routines/spectrum.py
@@ -1,31 +1,5 @@
 import numpy as np
 
-def compute_spectrum_gaussian(freqs: np.ndarray, freq_ctr: np.float, bw: np.float) -> np.ndarray:
-    """
-    Computes a one-dimensional frequency spectrum assuming a Gaussian shape.
-
-    Parameters
-    ----------
-    freqs : np.ndarray
-        an array of observing frequencies at which to evaluate spectrum
-
-    freq_ctr : float
-        central frequency of emission
-
-    bw : float
-        Gaussian width (i.e., bandwidth) of spectrum
-
-    Returns
-    -------
-    spectrum : np.ndarray
-        the one-dimensional spectrum for input frequencies
-    """
-
-    norm_factor = 1. / np.sqrt(2 * np.pi) / bw
-    spectrum = norm_factor * np.exp(-0.5 * ((freqs - freq_ctr) / bw)**2)
-    
-    return spectrum
-
 def compute_spectrum_rpl(freqs: np.ndarray, freq_ref: np.float, sp_idx: np.float, sp_run: np.float) -> np.ndarray:
     """
     Computes a one-dimensional frequency spectrum assuming the form of a running power law (rpl).
