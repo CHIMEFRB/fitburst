fix(profile): simplify and speed up the pbf calculation

fitburst/routines/profile.py
@@ -48,6 +48,7 @@ def compute_profile_gaussian(values: float, mean: float, width: float,
 
     return profile
 
+
 def compute_profile_pbf(time: float, toa: float, width: float, freq: float, ref_freq: float,
                         sc_time_ref: float, sc_index: float = -4., normalize: bool = False) -> float:
     """
@@ -92,23 +93,66 @@ def compute_profile_pbf(time: float, toa: float, width: float, freq: float, ref_
     ratio = width / sc_time
     arg1 = (ratio - z) / np.sqrt(2)
 
-    if normalize:
-        amp_term = np.sqrt(np.pi / 2) * ratio
-    else:
-        amp_term = (freq / ref_freq) ** (-sc_index)
+    amp_term = np.sqrt(np.pi / 2) * ratio if normalize else sc_time_ref / sc_time
 
-    with np.errstate(invalid="ignore", over="ignore"):
-        p1 = amp_term * np.exp(-0.5 * z ** 2) * ss.erfcx(arg1)
+    # Create the output array
+    shp = np.broadcast_shapes(freq.shape, time.shape)
+    profile = np.empty(shp, dtype=float)
 
-    # Use the old method when arg1 is less than roughly -20.0
-    # (corresponding to times much after the arrival time)
-    invalid = arg1 < -20.0
-    if np.any(invalid):
-        arg2 = (ratio / 2 - z) * ratio
-        p2 = amp_term * np.exp(arg2) * ss.erfc(arg1)
+    # Identify regimes where different calculations are needed to prevent numerical overflow
+    regime1, regime2 = _identify_pbf_regime(arg1)
 
-        profile = np.where(invalid, p2, p1)
-    else:
-        profile = p1
+    # Calculate the profile in first regime using the scaled complementary error function
+    if regime1 is not None:
+        profile[..., regime1] = amp_term * np.exp(-0.5 * z[..., regime1] ** 2) * ss.erfcx(arg1[..., regime1])
+
+    # Calculate the profile in second regime using the complementary error function
+    if regime2 is not None:
+        arg2 = (ratio / 2 - z[..., regime2]) * ratio
+        profile[..., regime2] = amp_term * np.exp(arg2) * ss.erfc(arg1[..., regime2])
 
     return profile
+
+
+def _identify_pbf_regime(arg: float, threshold=-20.0):
+    """Identify times where erfcx suffers from numerical overflow.
+
+    Parameters
+    ----------
+    arg : np.ndarray[nfreq, ntime] or np.ndarray[ntime,]
+        Argument to the scaled complementary error function erfcx.
+        In the context of the PBF model, this is given by:
+            (width / sc_time - (time - toa) / width) / np.sqrt(2)
+
+    threshold : float, optional
+        Values of arg less than this threshold will
+        be considered invalid.  Defaults to -20,
+        which works well for float64.
+
+    Returns
+    -------
+    valid : slice, array of int, or None
+        Indices in the time axis where the
+        scaled complimentary error function
+        can be used.  This will be None if
+        no time samples qualify.
+
+    invalid : slice, array of int, or None
+        Indices in the time axis where the
+        scaled complimentary error function
+        suffers from numerical overflow.
+        This will be None if no time samples
+        qualify.
+    """
+    flag = arg > threshold if arg.ndim == 1 else np.all(arg > threshold, axis=0)
+    valid = np.flatnonzero(flag)
+    invalid = np.flatnonzero(~flag) if valid.size < flag.size else None
+
+    # If possible, convert from indices to slices so that a copy does not occur
+    if (valid[-1] + 1 - valid[0]) == valid.size:
+        valid = slice(valid[0], valid[-1]+1)
+
+    if invalid is not None and (invalid[-1] + 1 - invalid[0]) == invalid.size:
+        invalid = slice(invalid[0], invalid[-1]+1)
+
+    return valid, invalid